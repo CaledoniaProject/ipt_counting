571933c616bc175faf1d4a7db65ece7a6eb1d9d0
CaledoniaProject <the.warl0ck.1989@gmail.com>
2013-01-02 23:01:35 +0800

    Initial Release

---
+++ ipt-count.c
@@ -0,0 +1,85 @@
+/*
+ *  Count how many rules in total are present, all tables
+ *
+ *  gcc -I include/ -I ../include/ ipt-count.c -liptc -lip4tc -lxtables -o ipt
+ *
+ *  sudo ./ipt
+ *
+ *  sudo ./ipt filter
+ *
+ * */
+
+#include <getopt.h>
+#include <sys/errno.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <netdb.h>
+#include <libiptc/libiptc.h>
+
+/* Debugging prototype. */
+static int for_each_table(int (*func)(const char *tablename))
+{
+	int count = 1;
+	FILE *procfile = NULL;
+	char tablename[255];
+
+	procfile = fopen("/proc/net/ip_tables_names", "re");
+	if (!procfile)
+		return 0;
+
+	while (fgets(tablename, sizeof(tablename), procfile)) {
+		if (tablename[strlen(tablename) - 1] != '\n')
+            continue;
+		tablename[strlen(tablename) - 1] = '\0';
+		count += func(tablename);
+	}
+
+	fclose(procfile);
+	return count;
+}
+
+
+static int do_output(const char *tablename)
+{
+	struct xtc_handle *h;
+	const char *chain = NULL;
+    int count = 0;
+
+	if (!tablename)
+		return for_each_table(&do_output);
+
+	h = iptc_init(tablename);
+	if (!h)
+    {
+        puts ("Error initializing iptc!");
+        return -1;
+    }
+
+	for (chain = iptc_first_chain(h);
+	     chain;
+	     chain = iptc_next_chain(h)) {
+		const struct ipt_entry *e;
+		e = iptc_first_rule(chain, h);
+		while(e) {
+            ++ count;
+			e = iptc_next_rule(e, h);
+		}
+	}
+
+	return count;
+}
+
+int main(int argc, char *argv[])
+{
+	int c;
+
+    if (argc == 2) 
+        printf ("%d\n", do_output (argv[1]));
+    else
+	    printf ("%d\n", do_output(NULL));
+
+    return 0;
+}
